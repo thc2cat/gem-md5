6bb74a098c69dba1631538226de76311d6930727
thc <thc2cat@gmail.com>
2024-09-06 16:47:57 +0200

    concurent version

--- main.go
+++ main.go
@@ -7,18 +7,32 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
+	"sync"
 
 	md5simd "github.com/minio/md5-simd"
 )
 
-var server = md5simd.NewServer()
+var (
+	server = md5simd.NewServer()
+	numCPU = runtime.NumCPU() * 8
+	input  = make(chan string, numCPU*10)
+	output = make(chan string, numCPU*10)
+)
 
 // Fonction pour afficher un chemin
 
 func main() {
 
-	// Liste des répertoires à parcourir
+	var wg, out sync.WaitGroup
+
+	for ; numCPU > 0; numCPU-- {
+		wg.Add(1)
+		go launchWorker(&wg)
+	}
+	out.Add(1)
+	go doOutput(&out)
 
 	roots, excludes := getOptions()
 	// Parcourir l'arborescence en utilisant filepath.Walk
@@ -28,6 +42,10 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Erreur lors du parcours de l'arborescence : %v\n", err)
 		}
 	}
+	close(input)
+	wg.Wait()
+	close(output)
+	out.Wait()
 }
 
 func getOptions() (dirPtr, exclude string) {
@@ -60,7 +78,7 @@ func walk(dirPtr, exclude string) error {
 	return filepath.Walk(dirPtr, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			if os.IsNotExist(err) {
-				fmt.Fprintf(os.Stderr, "Le fichier %s n'existe plus.\n", path)
+				fmt.Fprintf(os.Stderr, "Le fichier %s n'existe pas.\n", path)
 				return nil // On ignore cette erreur et on continue
 			} else if os.IsPermission(err) {
 				fmt.Fprintf(os.Stderr, "Accès refusé au répertoire : %s\n", path)
@@ -75,18 +93,52 @@ func walk(dirPtr, exclude string) error {
 			if matched {
 				return nil
 			}
-			size, serr := getSize(path)
-			if serr != nil || size == 0 {
-				return nil
-			}
-			S, E := getHash(path)
-			if E == nil {
-				fmt.Printf("%s:%d:%s\n", S, size, path)
-			}
+			input <- path
+			return nil
 		}
 		return nil
 	})
 }
+
+func doOutput(mywg *sync.WaitGroup) {
+	for st := range output {
+		fmt.Print(st)
+	}
+	mywg.Done()
+}
+func launchWorker(wg *sync.WaitGroup) {
+	for st := range input {
+		S, err := performS(st)
+		if err == nil {
+			output <- S
+		}
+	}
+	wg.Done()
+}
+func performS(path string) (string, error) {
+	size, serr := getSize(path)
+	if serr != nil || size == 0 {
+		return "", serr
+	}
+	S, E := getHash(path)
+	if E != nil {
+		return "", E
+	}
+
+	return fmt.Sprintf("%s:%d:%s\n", S, size, path), E
+}
+
+// func perform(path string) error {
+// 	size, serr := getSize(path)
+// 	if serr != nil || size == 0 {
+// 		return nil
+// 	}
+// 	S, E := getHash(path)
+// 	if E == nil {
+// 		fmt.Printf("%s:%d:%s\n", S, size, path)
+// 	}
+// 	return E
+// }
 
 func getHash(thePath string) (string, error) {
 
